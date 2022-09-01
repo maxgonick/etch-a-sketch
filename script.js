8fbd3d20aa5bd6db4c4ac04b12a15c9f95b8a2e5
maxgonick <maxwellgonick@g.ucla.edu>
2022-09-01 16:17:36 -0700

    grid layout with fixed overlapping borders

--- script.js
+++ script.js
@@ -1,10 +1,43 @@
 const gridContainer = document.querySelector(".grid-container");
 
 //Create divs
-for (let i = 0; i < 16; i++) {
+for (let i = 0; i < 4; i++) {
   let temp = document.createElement("div");
   gridContainer.appendChild(temp);
-  temp.style.width = "2vw";
-  temp.style.minHeight = "2vh";
-  temp.style.border = "0.125rem solid";
+  temp.style.width = "12vw";
+  temp.style.minHeight = "12vh";
+  temp.classList.add("grid-child");
+  temp.style.borderTop = "0.0625rem solid";
+  temp.style.borderLeft = "0.0625rem solid";
+  temp.style.borderBottom = "0.0625rem solid";
+}
+
+for (let i = 0; i < 4; i++) {
+  let temp = document.createElement("div");
+  gridContainer.appendChild(temp);
+  temp.style.width = "12vw";
+  temp.style.minHeight = "12vh";
+  temp.classList.add("grid-child");
+  temp.style.borderLeft = "0.0625rem solid";
+  temp.style.borderBottom = "0.0625rem solid";
+}
+
+for (let i = 0; i < 4; i++) {
+  let temp = document.createElement("div");
+  gridContainer.appendChild(temp);
+  temp.style.width = "12vw";
+  temp.style.minHeight = "12vh";
+  temp.classList.add("grid-child");
+  temp.style.borderLeft = "0.0625rem solid";
+  temp.style.borderBottom = "0.0625rem solid";
+}
+
+for (let i = 0; i < 4; i++) {
+  let temp = document.createElement("div");
+  gridContainer.appendChild(temp);
+  temp.style.width = "12vw";
+  temp.style.minHeight = "12vh";
+  temp.classList.add("grid-child");
+  temp.style.borderLeft = "0.0625rem solid";
+  temp.style.borderBottom = "0.0625rem solid";
 }
